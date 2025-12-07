3f25a15f5823a24b87932c8da3953924c0982d2e
TOMAS ALTERMATT <altermatt.tomas@gmail.com>
2025-12-07 16:12:24 +0100

    separate 2 Trajectories dynamics

--- dynamics_PRO_Trajectory2.m
+++ dynamics_PRO_Trajectory2.m
@@ -3,63 +3,63 @@ addpath('Functions')
 
 %% Data
 % Base Parameters
-Rb = 10e-2;
-Lb = 10e-2;
-m_base = 1.941; % [kg]
-g_base_vector = [0.384	22.472	134.03]*10^-3;
+Rb = 9.4e-2;
+Lb = 18.1e-2;
+m_base = 2.028; % [kg]
+g_base_vector = [0.404	24.135	136.166]*10^-3;
 g_base = norm(g_base_vector);
-Ib_tot = [11418.618	-29.07	    -34.147;
-        -29.07	    9784.946	-2022.797;
-        -34.147	    -2022.797	4677.412]*10^-6;
+Ib_tot = [11767.657	-31.389	    -36.140;
+        -31.389	    10002.276	-2169.368;
+        -36.140	    -2169.368	4842.399]*10^-6;
 Ib_mom = [Ib_tot(1,1), Ib_tot(2,2), Ib_tot(3,3)];
 Ib_prod = [Ib_tot(1,2), Ib_tot(1,3), Ib_tot(2,3)];
 
 
 % Link 1 arm Parameters
-a1 = 329.6e-3;             % length
-d1 = 4e-2;              % height
-w1 = 45.884e-3;              % width
-m1 = 1.517;       % link mass
-g1_vector = [268.116  31.803 -0.435]*10^-3;
+a1 = 341.000e-3;             % length
+d1 = 6e-2;              % height
+w1 = 45.3e-3;              % width
+m1 = 1.693;       % link mass
+g1_vector = [267.188 23.527 0.009]*10^-3;
 g1_ss = g1_vector - ([a1, 0, 0])/2;
 
-I1_tot = [2308.896	-4597.06	40.075
-        -4597.06	19271.641	30.259
-        40.075	    30.259	    21009.606]*10^-6;
+I1_tot = [2817.648	-3945.008	6.873
+        -3945.008	19454.068	1.668	
+        6.873	    1.668	    21458.668]*10^-6;
 
 I1_mom = [I1_tot(1,1), I1_tot(2,2), I1_tot(3,3)];
 I1_prod = [I1_tot(1,2), I1_tot(1,3), I1_tot(2,3)];
 
 
 % Link 2 arm Parameters
-a2 = 269.6e-3;        % length
-d2 = 4e-2;          % height
-w2 = 45.884e-3;          % width
+a2 = 272.000e-3;        % length
+d2 = 6e-2;          % height
+w2 = 45.3e-3;          % width
 
-m2 = 1.141;             % link mass
-g2_vector = [231.567  -30.006	-0.012]*10^-3;
+m2 = 1.332;             % link mass
+g2_vector = [216.754  -19.494	-0.011]*10^-3;
 g2_ss = g2_vector - ([a2, 0, 0])/2;
 
-I2_tot = [1077.164	1787.595	-4.267
-        1787.595	7556.209	1.777
-        -4.267	    1.777	    8296.218]*10^-6;
+I2_tot = [1593.165	1562.049	-4.184 
+        1562.049 	8445.514	1.777
+        -4.184 	    1.656	    9424.927]*10^-6;
 I2_mom = [I2_tot(1,1), I2_tot(2,2), I2_tot(3,3)];
 I2_prod = [I2_tot(1,2), I2_tot(1,3), I2_tot(2,3)];
 
 
 
 % Link 3 arm Parameters
-a3 = 182.6e-3;        % length
-d3 = 4e-2;          % height
+a3 = 181e-3;        % length
+d3 = 6e-2;          % height
 w3 = 45.884e-3;          % width
-m3 = 0.181;             % link mass
-g3_vector = [98.46	-4.791	-0.006]*10^-3;
+m3 = 0.167;             % link mass
+g3_vector = [95.106	-3.521	-0.006]*10^-3;
 g3_ss = g3_vector - ([a3, 0, 0])/2;
 
 % Calculate the inertia matrix for Link 3
-I3_tot = [47.434	-55.93	0.024
-          -55.93	575.393	-0.007
-           0.024	-0.007	563.653]*10^-6;
+I3_tot = [41.367	-40.381	0.018
+          -40.381	492.346 	-0.008
+           0.018	-0.008	479.349]*10^-6;
 
 I3_mom = [I3_tot(1,1), I3_tot(2,2), I3_tot(3,3)];
 I3_prod = [I3_tot(1,2), I3_tot(1,3), I3_tot(2,3)];
@@ -127,6 +127,7 @@ motor.A = [1; 1; 1; 1]*10^1;
 motor.D = [1; 1; 1; 1]*5*10^0;
 motor.V = [1; 1; 1; 1]*10^2;
 
+%[joint_positions, joint_velocities, joint_accelerations, time_vect] = PROlines_parabolas(Q_seq, n_joints, motor);
 [joint_positions, joint_velocities, joint_accelerations, time_vect] = PROcubic_splines(Q_seq, n_joints, motor);
 
 
@@ -190,7 +191,7 @@ q_ddot_ts_j3 = timeseries(joint_accelerations(3,:), time_vect, 'Name', 'Joint3Ac
 q_ddot_ts_j4 = timeseries(joint_accelerations(4,:), time_vect, 'Name', 'Joint4Acceleration');
 
 
-out = sim('DOF4_v1.slx');
+out = sim('DOF4_v2.slx');
 
 C1 = out.J1_torque;
 C2 = out.J2_torque;
