882b1d4f1df74141fabf861e5d0dfc3f6c2c02bf
TOMAS ALTERMATT <altermatt.tomas@gmail.com>
2025-12-08 23:37:40 +0100

    dynamics with corrected ADE trajectory

--- dynamics_PRO.m
+++ dynamics_PRO.m
@@ -28,7 +28,7 @@ I1_tot = [2817.648	-3945.008	6.873
         6.873	    1.668	    21458.668]*10^-6;
 
 I1_mom = [I1_tot(1,1), I1_tot(2,2), I1_tot(3,3)];
-I1_prod = [I1_tot(1,2), I1_tot(1,3), I1_tot(2,3)];
+I1_prod = [I1_tot(2,3), I1_tot(1,3), I1_tot(1,2)];
 
 
 % Link 2 arm Parameters
@@ -44,14 +44,14 @@ I2_tot = [1593.165	1562.049	-4.184
         1562.049 	8445.514	1.777
         -4.184 	    1.656	    9424.927]*10^-6;
 I2_mom = [I2_tot(1,1), I2_tot(2,2), I2_tot(3,3)];
-I2_prod = [I2_tot(1,2), I2_tot(1,3), I2_tot(2,3)];
+I2_prod = [I2_tot(2,3), I2_tot(1,3), I2_tot(1,2)];
 
 
 
 % Link 3 arm Parameters
 a3 = 181e-3;        % length
 d3 = 6e-2;          % height
-w3 = 45.884e-3;          % width
+w3 = 45.3e-3;          % width
 m3 = 0.167;             % link mass
 g3_vector = [95.106	-3.521	-0.006]*10^-3;
 g3_ss = g3_vector - ([a3, 0, 0])/2;
@@ -62,7 +62,8 @@ I3_tot = [41.367	-40.381	0.018
            0.018	-0.008	479.349]*10^-6;
 
 I3_mom = [I3_tot(1,1), I3_tot(2,2), I3_tot(3,3)];
-I3_prod = [I3_tot(1,2), I3_tot(1,3), I3_tot(2,3)];
+I3_prod = [I3_tot(2,3), I3_tot(1,3), I3_tot(1,2)];
+
 
 
 % Save 3 inertia tensors
@@ -124,9 +125,10 @@ Q_seq = [];
 
 % End Effector Position (Initial & Final)
 S1 = [0; 30e-2; 5e-2; -pi/2];
-S8 = [0; 40e-2; 5e-2; -pi/4];
+
 
 if trajectory == 1
+    S8 = [0; 40e-2; 5e-2; -pi/4];
     seq = Trajectory_1(0.15, 0.18, 10, S1, S8);
     for i=1:length(seq(1,:))
         Q_seq(:,i) = PROinv2(seq(:,i), L(1:3), -1);
@@ -134,6 +136,7 @@ if trajectory == 1
     [joint_positions, joint_velocities, joint_accelerations, time_vect] = PROlines_parabolas(Q_seq, n_joints, motor);
 
 elseif trajectory == 2
+    S8 = [0; 40e-2; 14e-2; -pi/4];
     seq = Trajectory_2(0.15, 0.18, 10, S1, S8);
     for i=1:length(seq(1,:))
         Q_seq(:,i) = PROinv2(seq(:,i), L(1:3), -1);
@@ -159,8 +162,8 @@ for i = 1:length(time_vect)
     S_vec(:,i) = PROdir(Q, L);
     
     % Extended Jacobians
-    Je = PROjacdinV2(Q, L);
-    Jep = PROjacPdinV2(Q, Qp, L);
+    Je = PROjacdinV2(Q, L, 0);
+    Jep = PROjacPdinV2(Q, Qp, L, 0);
     Spp = Jep*Qp + Je*Qpp;
     
     % Spring Contribution
