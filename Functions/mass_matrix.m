dd3c5b66a81ea644069a2ee8d990f5df1d4f760d
TOMAS ALTERMATT <altermatt.tomas@gmail.com>
2025-12-08 23:37:01 +0100

    add offset to jacobians

--- Functions/mass_matrix.m
+++ Functions/mass_matrix.m
@@ -9,10 +9,11 @@ function [mass_matrix] = mass_matrix(m_pl, J_pl, J_rob, m_rob)
     m2 = m_rob(2);
     m3 = m_rob(3);
 
-    % Link Inertia Tensors (may change to vectors
+    % Link Inertia Tensors
     J1 = J_rob(:,:,1);
     J2 = J_rob(:,:,2);
     J3 = J_rob(:,:,3);
+    Jbase = J_rob(:,:,4);
     
     % Payload mass contribution
     mass_matrix(1:3, 1:3) = m_pl*eye(3);
@@ -28,8 +29,10 @@ function [mass_matrix] = mass_matrix(m_pl, J_pl, J_rob, m_rob)
     mass_matrix(12,12) = J2(3,3);       % psi
     mass_matrix(16, 16) = J_pl(1,1) + J1(1,1) + J2(1,1) + J3(1,1);
     
-    J_tot = J_pl(1,1) + J1(1,1) + J2(1,1) + J3(1,1);
+    J_tot = J_pl + J1 + J2 + J3 + Jbase;
+    
     % Simplified Case
-    mass_matrix = diag([m_pl, m_pl, m_pl, J_pl(3,3) + J3(3,3), m1, m1, m1, J1(3,3), m2, m2, m2, J2(3,3), m3, m3, m3, J_tot]);
+    mass_matrix = diag([m_pl, m_pl, m_pl, J_pl(3,3) + J3(3,3), m1, m1, m1, J1(3,3), m2, m2, m2, J2(3,3), m3, m3, m3, J_tot(2,2)]);
+    %mass_matrix = diag([m_pl, m_pl, m_pl, J_pl(2,2) + J3(2,2), m1, m1, m1, J1(2,2), m2, m2, m2, J2(2,2), m3, m3, m3, J_tot(3,3)]);
 
 end
